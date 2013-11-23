d0b0880abf4d4ebb6018b68e314e3e25b25183ff
Will Song <incertia9474@gmail.com>
2013-11-22 20:22:27 -0600

    removed const because C doesn't really do const

--- keccak.c
+++ keccak.c
@@ -15,7 +15,7 @@
 #endif /* #ifdef KECCAK_ROUNDS */
 #define KECCAK_ROUNDS	24
 
-const uint64_t keccakf_rndc[24] =
+uint64_t keccakf_rndc[24] =
 {
 	0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
 	0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
@@ -27,13 +27,13 @@ const uint64_t keccakf_rndc[24] =
 	0x8000000000008080, 0x0000000080000001, 0x8000000080008008,
 };
 
-const uint64_t keccakf_rotc[24] =
+uint64_t keccakf_rotc[24] =
 {
 	1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
 	27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44,
 };
 
-const uint64_t keccakf_piln[24] =
+uint64_t keccakf_piln[24] =
 {
 	10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
 	15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1,
@@ -77,7 +77,7 @@ void keccakf(uint64_t s[25], int rounds){
 	}
 }
 
-void keccak(const uint8_t *in, const size_t len, uint8_t *hash, const size_t hashlen){
+void keccak(uint8_t *in, size_t len, uint8_t *hash, size_t hashlen){
 	uint64_t state[25];
 	uint8_t tmp[144];
 	register int i, rsize, rsizew;
--- keccak.h
+++ keccak.h
@@ -12,7 +12,7 @@
 #include <stddef.h>
 
 /* Computes a keccak hash */
-void keccak(const uint8_t *in, const size_t len, uint8_t* hash, const size_t hashlen);
+void keccak(uint8_t *in, size_t len, uint8_t* hash, size_t hashlen);
 
 
 #endif /* #ifndef KECCAK_H */
