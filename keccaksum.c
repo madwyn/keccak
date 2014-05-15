e8547f210d5962d4c27071cf054c52e3bc28c74d
Will Song <incertia9474@gmail.com>
2014-05-14 20:27:37 -0500

    now files are read by chunks, and stdin should work

--- keccaksum.c
+++ keccaksum.c
@@ -12,6 +12,44 @@
 
 #include "keccak.h"
 
+/* Get rid of all previous definitions of ROTL64 */
+#ifdef ROTL64
+#undef ROTL64
+#endif /* #ifdef ROTL64 */
+#define ROTL64(x, y)	(((x) << (y)) | ((x) >> (64 - (y))))
+
+/* Get rid of all previous definitions of KECCAK_ROUNDS */
+#ifdef KECCAK_ROUNDS
+#undef KECCAK_ROUNDS
+#endif /* #ifdef KECCAK_ROUNDS */
+#define KECCAK_ROUNDS	24
+
+const uint64_t keccakf_rndc[24] =
+{
+	0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
+	0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
+	0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
+	0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+	0x000000008000808b, 0x800000000000008b, 0x8000000000008089,
+	0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
+	0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
+	0x8000000000008080, 0x0000000080000001, 0x8000000080008008,
+};
+
+const uint64_t keccakf_rotc[24] =
+{
+	1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
+	27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44,
+};
+
+const uint64_t keccakf_piln[24] =
+{
+	10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
+	15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1,
+};
+
+void keccakf(uint64_t _s[25], const int _rounds);
+
 void printsum(const char *_fname, FILE *_f, size_t size);
 
 /* USAGE: keccaksum <digest_size> <files> */
@@ -29,7 +67,8 @@ int main(int argc, char **argv){
 	} else {
 		sscanf(argv[1], "%lu", &digestsize);
 		for(i = 2; i < argc; i++){
-			if(access(argv[i], F_OK) != 0) fprintf(stderr, "Error: could not find %s\n", argv[i]);
+			if(strcmp(argv[i], "-") == 0) printsum("-", stdin, digestsize / 8);
+			else if(access(argv[i], F_OK) != 0) fprintf(stderr, "Error: could not find %s\n", argv[i]);
 			else if(access(argv[i], R_OK) != 0) fprintf(stderr, "Error: cannot read %s\n", argv[i]);
 			else printsum(argv[i], fopen(argv[i], "r"), digestsize / 8);
 		}
@@ -38,32 +77,96 @@ int main(int argc, char **argv){
 	return 0;
 }
 
-void printsum(const char *_fname, FILE *_f, size_t size){
-	register long len;
-	register size_t i;
-	register char *buf = NULL;
-	uint8_t *hash = malloc(sizeof(char) * size);
+/* Updates the given state with a given number of rounds */
+void keccakf(uint64_t _s[25], const int _rounds){
+	register int i, j, round;
+	register uint64_t t;
+	uint64_t bc[5];
 
-	fseek(_f, 0, SEEK_END);
-	len = ftell(_f);
-	fseek(_f, 0, SEEK_SET);
+	for(round = 0; round < _rounds; round++){
+		//Theta step
+		for(i = 0; i < 5; i++)
+			bc[i] = _s[i] ^ _s[i + 5] ^ _s[i + 10] ^ _s[i + 15] ^ _s[i + 20];
+		for(i = 0; i < 5; i++){
+			t = bc[(i + 4) % 5] ^ ROTL64(bc[(i + 1) % 5], 1);
+			for(j = 0; j < 25; j += 5)
+				_s[j + i] ^= t;
+		}
 
-	buf = malloc(sizeof(char) * len);
-	memset(buf, 0, sizeof(char) * len);
+		//Rho and Pi
+		t = _s[1];
+		for(i = 0; i < 24; i++){
+			j = keccakf_piln[i];
+			bc[0] = _s[j];
+			_s[j] = ROTL64(t, keccakf_rotc[i]);
+			t = bc[0];
+		}
 
-	if(!buf){
-		fprintf(stderr, "Error: not enough memory to load %s\n", _fname);
-		goto fin;
+		//Chi
+		for(j = 0; j < 25; j += 5){
+			for(i = 0; i < 5; i++)
+				bc[i] = _s[j + i];
+			for(i = 0; i < 5; i++)
+				_s[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
+		}
+
+		//Iota
+		_s[0] ^= keccakf_rndc[round];
 	}
+}
 
-	fread(buf, sizeof(char), len, _f);
-	//printf("%s", buf);
-	keccak((uint8_t *)buf, len, hash, size);
+void printsum(const char *_fname, FILE *_f, size_t size){	
+	uint64_t state[25];
+	uint8_t *hash = malloc(size * sizeof(uint8_t));
+	uint8_t buf[25];
+	uint8_t tmp[144];
+	register int i, rsize, rsizew;
+	register size_t j;
+
+	rsize = 200 - 2 * size;
+	rsizew = rsize / 8;
+
+	//Clear the state
+	memset(state, 0, sizeof(state));
+
+	do {
+		//read up to rsize bytes, then do work
+		j = fread(buf, 1, rsize, _f);
+		
+		//check some stuff
+		if(feof(_f)){
+			break;
+		} else if(ferror(_f)){
+			fprintf(stderr, "Error when reading %s.\n", _fname);
+			goto fin;
+		} else {
+			//First few blocks (i.e. not last block)
+			for(; j >= rsize; j -= rsize){
+				for(i = 0; i < rsizew; i++)
+					state[i] ^= ((uint64_t *)buf)[i];
+				keccakf(state, KECCAK_ROUNDS);
+			}
+		}
+	} while(1);
+
+	//Last block + padding
+	memcpy(tmp, buf, j);
+	tmp[j++] = 1;
+	memset(tmp + j, 0, rsize - j);
+	tmp[rsize - 1] |= 0x80;
+
+	for(i = 0; i < rsizew; i++)
+		state[i] ^= ((uint64_t *)tmp)[i];
+	keccakf(state, KECCAK_ROUNDS);
+
+	//copy hash
+	memcpy(hash, state, size);
+
+	//print
 	for(i = 0; i < size; i++) printf("%02x", hash[i]);
 	printf("  %s\n", _fname);
 
-fin:
+fin:	
 	if(_f != stdin) fclose(_f);
 	free(hash);
-	free(buf);
 }
