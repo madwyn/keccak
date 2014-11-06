e62b6c8c441893091f9a39403e903aa9fc1b1a7d
madwyn <madwyn@gmail.com>
2014-11-06 14:56:18 +0000

    make it work in C++

--- keccak.h
+++ keccak.h
@@ -11,9 +11,17 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Computes a keccak hash 
  * Also const correctness is a headache */
 void keccak(const uint8_t *_in, const size_t _len, uint8_t *const _hash, const size_t _hashlen);
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* #ifndef KECCAK_H */
+
